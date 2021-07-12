0b3938b1c89a3d34630e6bb399c9edbb36a27e91
Douglas Emiliano <emaildedouglas@gmail.com>
2021-07-12 01:21:30 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule, ReactiveFormsModule }  from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
+import { PaginaComponent } from './pagina/pagina.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PaginaComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/pagina/pagina.component.ts
@@ -0,0 +1,79 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+
+
+
+@Component({
+  selector: 'app-pagina',
+  templateUrl: './pagina.component.html',
+  styleUrls: ['./pagina.component.scss']
+})
+export class PaginaComponent implements OnInit {
+
+  resultado = 0;
+  anterior = 0;
+  show = true;
+  form!: FormGroup
+  label: any;
+  labelIMG = "Esconder";
+  matriz = [{ "id": 1, "content": "Olá, sou o filtro numero 1!"},
+            { "id": 2, "content": "Helo, sou o filtro numero 2!"},
+            { "id": 3, "content": "Capiche, sou o filtro numero 3!"},
+            { "id": 4, "content": "Hi, sou o filtro numero 4!"},
+            { "id": 5, "content": "Meu nome é Douglas, sou o filtro numero 5!"},
+            { "id": 6, "content": "Ciência da Computação, sou o filtro numero 6!"},
+            { "id": 7, "content": "Não entendi muito bem essa ultima parte, sou o filtro numero 7!"},
+            { "id": 8, "content": "Tudo bem? sou o filtro numero 8!"},
+            { "id": 9, "content": "Tchau, sou o filtro numero 9!"},
+            { "id": 10,"content": "Foi bom participar, sou o filtro numero 10!"}];
+
+  constructor(private fb: FormBuilder) {
+   }
+
+  ngOnInit(): void {
+    this.createForm();
+  }
+
+  createForm(){
+    this.form = this.fb.group({
+      filter: new FormControl('', [Validators.required, Validators.minLength(1), Validators.maxLength(2), Validators.nullValidator]),
+    });
+
+  }
+
+  calcularFibonacci(): void {
+    if(this.resultado == 0){
+      this.resultado = 1;
+    } else if (this.resultado == 1){
+      this.anterior = this.resultado;
+      this.resultado = this.resultado + this.anterior;
+    }else {
+      this.resultado = this.resultado + this.anterior;
+      this.anterior = this.resultado - this.anterior;
+      }
+    }
+
+    resetar(): void{
+      this.anterior = 0;
+      this.resultado = 0;
+    }
+
+  esconder(): void{
+    if (this.show){
+      this.show = false;
+      this.labelIMG = "Esconder";
+    }else {
+      this.show = true;
+      this.labelIMG = "Mostrar";
+    }
+  }
+
+  onSubmit(){
+    if (this.form.value.filter > 10){
+      this.label = "Digite um número de 0 a 10"
+    } else {
+      this.label =  this.matriz[this.form.value.filter -1].content;
+    }
+    this.form.reset();
+  }
+}
